4290014bc3235669b1022347b431d133a86dcd1d
Yuval Dadon <yuvald@moveo.co.il>
2025-06-23 12:32:19 +0300

    HeroSearch - fix pp desktop & mobile

--- src/cmps/HeroSearch.tsx
+++ src/cmps/HeroSearch.tsx
@@ -1,17 +1,20 @@
+import { useMedia } from "../hooks/useMedia";
 import styles from "/src/assets/styles/cmps/HeroSearch.module.scss";
 
 
 export const HeroSearch = () => {
+    const media = useMedia();
+
     return (
-        <section className = {styles.heroSearch}>
-            <div className = {styles.content}>
-                <h2 className = {styles.headline}>
+        <section className={styles[`heroSearch--${media}`]}>
+            <div className={styles[`content--${media}`]}>
+                <h1 className={styles.headline}>
                     Epicure works with the top chef restaurants in Tel Aviv
-                </h2>
+                </h1>
                 <input
-                type = "text"
-                placeholder = "Search for restaurant cuisine, chef"
-                className = {styles.input}
+                    type="text"
+                    placeholder="Search for restaurant cuisine, chef"
+                    className={styles[`input--${media}`]}
                 />
             </div>
         </section>
