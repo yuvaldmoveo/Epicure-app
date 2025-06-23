c73e211b0f1b4440c0ba3c24be9c91d76ad68e9f
Yuval Dadon <yuvald@moveo.co.il>
2025-06-23 16:28:04 +0300

    fix - mobile pp

--- src/cmps/DishCard.tsx
+++ src/cmps/DishCard.tsx
@@ -2,7 +2,6 @@ import type { Media } from '../hooks/useMedia'
 import type { Dish } from '../types'
 import styles from '../assets/styles/cmps/DishCard.module.scss'
 import { renderIcons } from '../utils/renderIcons'
-import { renderPrice } from '../utils/renderPrice'
 
 interface DishCardProps {
   data: Dish
@@ -11,47 +10,59 @@ interface DishCardProps {
 
 export const DishCard = ({ data, media }: DishCardProps) => {
   const { name, ingredients, pictureUrl, price, icons } = data
+  const formattedName = name.replace(' ', '\n');
 
   if (!media) return null
+
   return (
     <article className={styles[`dish-card--${media}`]}>
       <img src={pictureUrl} alt={name} className={styles.image} />
       <div className={styles.data}>
         {media === 'desktop' ? (
           <>
-            <h3 className={styles.name}>{name}</h3>
-            {renderIcons({
-              icon: icons,
-              media,
-              iconBoxClass: styles.iconBox,
-              iconClass: styles.icon,
-            })}
-            <p className={styles.ingredients}>{ingredients}</p>
-            {renderPrice({
-              price,
-              priceBoxClass: styles.priceBox,
-              lineClass: styles.line,
-              priceClass: styles.price,
-              ilsIconClass: styles.ilsIcon,
-            })}
+            <div className={styles.textBox}>
+              <h3 className={styles.name}>{formattedName}</h3>
+              {renderIcons({
+                icon: icons,
+                media,
+                iconBoxClass: styles.iconBox,
+                iconClass: styles.icon,
+              })}
+              <p className={styles.ingredients}>{ingredients}</p>
+            </div>
+
+            <div className={styles.priceWrapper}>
+              <span className={styles.line}></span>
+              <div className={styles.priceBox}>
+                {renderIcons({
+                  icon: "ILS",
+                  iconClass: styles.ilsIcon,
+                })}
+                <span className={styles.priceText}>{price}</span>
+              </div>
+              <span className={styles.line}></span>
+            </div>
           </>
         ) : (
           <>
-            <h3 className={styles.name}>{name}</h3>
-            <p className={styles.ingredients}>{ingredients}</p>
-            {renderIcons({
-              icon: icons,
-              media,
-              iconBoxClass: styles.iconBox,
-              iconClass: styles.icon,
-            })}
-            {renderPrice({
-              price,
-              priceBoxClass: styles.priceBox,
-              lineClass: styles.line,
-              priceClass: styles.price,
-              ilsIconClass: styles.ilsIcon,
-            })}
+            <div className={styles.textBox}>
+              <h3 className={styles.name}>{formattedName}</h3>
+              <p className={styles.ingredients}>{ingredients}</p>
+              {renderIcons({
+                icon: icons,
+                media,
+                iconBoxClass: styles.iconBox,
+                iconClass: styles.icon,
+              })}
+            </div>
+
+            <div className={styles.priceWrapper}>
+              {renderIcons({
+                icon: "ILS",
+                iconClass: styles.ilsIcon,
+              })}
+              <span className={styles.priceText}>{price}</span>
+            </div>
           </>
         )}
       </div>
